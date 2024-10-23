0b3d7528186a9739f5f5987eda5c0b0d0140b995
Isma <ismenchucruck69@gmail.com>
2024-10-24 01:02:50 +0200

    Delete MANEJO DE FICHEROS directory

--- MANEJO DE FICHEROS/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ejercicio2_3_1"}];updateSearchResults();
--- MANEJO DE FICHEROS/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ejercicio2_3_1","l":"manejoficheros"}];updateSearchResults();
